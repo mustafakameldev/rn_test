a04ff06db6a577636e527ebd8cf62d9a1beae335
mospro <mustafakameldev@gmail.com>
2021-06-22 12:27:12 +0200

    factor:testComponent

--- src/screens/tests/SignIn.test.js
+++ src/screens/tests/SignIn.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { render, fireEvent, act } from '@testing-library/react-native';
 import jest from 'jest-mock';
 import SignIn from '../SignIn';
-
 const flushMicrotasksQueue = () =>
   new Promise((resolve) => setImmediate(resolve));
 it('render default elemetns ', () => {
@@ -11,9 +10,25 @@ it('render default elemetns ', () => {
   getByPlaceholderText('type username');
   getByPlaceholderText('type password');
 });
-it('shows invalid user name error message', () => {
-  const { getByTextId, getByText, queryAllByText } = render(<SignIn />);
-  fireEvent.changeText(getByTextId('SignIn.passwordInput'), '00000');
-  fireEvent.press(getByTextId('SignIn.Button'));
+it('Show invalid input messages ', () => {
+  const { getByTestId, getByText } = render(<SignIn />);
+  fireEvent.press(getByTestId('SignIn.Button'));
   getByText('Invalid username.');
+  getByText('Invalid password.');
+});
+it('shows invalid user name error message', () => {
+  const { getByTestId, getByText, queryAllByText } = render(<SignIn />);
+  fireEvent.changeText(getByTestId('SignIn.passwordInput'), '00000');
+  fireEvent.press(getByTestId('SignIn.Button'));
+  getByText('Invalid username.');
+  expect(queryAllByText('invalid passsword.').length).toBe(0);
+  fireEvent.changeText(getByTestId('SignIn.usernameInput'), 'invalid input');
+  getByText('Invalid username.');
+  expect(queryAllByText('invalid password.').length).toBe(0);
+});
+it('Show  invalid password error message', () => {
+  const { getByTestId, getByText, queryAllByText } = render(<SignIn />);
+  fireEvent.changeText(getByTestId('SignIn.usernameInput'), 'example');
+  fireEvent.press(getByTestId('SignIn.Button'));
+  getByText('Invalid password.');
 });
